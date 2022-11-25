d55f89caa3dc38ddc92cdc3a3d6a3ec2c91f85f9
Russellg <103833866+RussellGN@users.noreply.github.com>
2022-11-25 14:30:21 +0200

    finally made that update

--- assets/js/custom.js
+++
@@ -1,8 +0,0 @@
-myNavLinks = document.querySelectorAll(".my-nav-link");
-offcanvasClose = document.getElementById("offcanvas-close");
-
-myNavLinks.forEach((el) => {
-	el.onclick = function () {
-		offcanvasClose.click();
-	};
-});
