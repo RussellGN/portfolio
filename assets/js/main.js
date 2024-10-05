cd4543a7ff552b1cf6dda23828e4c0574a6588c9
Russellg <dudurussell@gmail.com>
2024-10-05 11:49:03 +0200

    feat: restructure and update site

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,85 +1,72 @@
 (function () {
-	("use strict");
+   ("use strict");
 
-	/* Easy selector helper function */
-	const select = (el, all = false) => {
-		el = el.trim();
-		if (all) {
-			return [...document.querySelectorAll(el)];
-		} else {
-			return document.querySelector(el);
-		}
-	};
+   /* Easy selector helper function */
+   const select = (el, all = false) => {
+      el = el.trim();
+      if (all) {
+         return [...document.querySelectorAll(el)];
+      } else {
+         return document.querySelector(el);
+      }
+   };
 
-	/* Preloader */
-	let preloader = select("#preloader");
-	if (preloader) {
-		window.addEventListener("load", () => {
-			preloader.remove();
-		});
-	}
+   /* Preloader */
+   let preloader = select("#preloader");
+   if (preloader) {
+      window.addEventListener("load", () => {
+         preloader.remove();
+      });
+   }
 
-	/* Hero type effect */
-	const typed = select(".typed");
-	if (typed) {
-		let typed_strings = typed.getAttribute("data-typed-items");
-		typed_strings = typed_strings.split(",");
-		new Typed(".typed", {
-			strings: typed_strings,
-			loop: true,
-			typeSpeed: 100,
-			backSpeed: 50,
-			backDelay: 2000,
-		});
-	}
+   select(".my-nav-link", true).forEach((link) => {
+      link.onclick = (e) => {
+         e.preventDefault();
+         document.getElementById("offcanvas-close").click();
+         if (e.target.hash === "#home") {
+            window.scrollTo(0, 0);
+            return;
+         }
 
-	/* Skills animation */
-	let skilsContent = select(".skills-content");
-	if (skilsContent) {
-		new Waypoint({
-			element: skilsContent,
-			offset: "80%",
-			handler: function (direction) {
-				let progress = select(".progress .progress-bar", true);
-				progress.forEach((el) => {
-					el.style.width = el.getAttribute("aria-valuenow") + "%";
-				});
-			},
-		});
-	}
+         let menuButtonContainer = select("#menu-button-container");
+         let offset = menuButtonContainer.offsetHeight;
 
-	select(".my-nav-link", true).forEach((link) => {
-		link.onclick = (e) => {
-			e.preventDefault();
-			document.getElementById("offcanvas-close").click();
-			if (e.target.hash === "#home") {
-				window.scrollTo(0, 0);
-				return;
-			}
+         let elementPos = document.querySelector(e.target.hash).offsetTop;
 
-			let menuButtonContainer = select("#menu-button-container");
-			let offset = menuButtonContainer.offsetHeight;
+         window.scrollTo({
+            top: elementPos - offset,
+            behavior: "smooth",
+         });
+      };
+   });
 
-			let elementPos = document.querySelector(e.target.hash).offsetTop;
+   /* Scroll to background button */
+   document.querySelector(".btn-reb").addEventListener("click", (e) => {
+      e.preventDefault();
+      let menuButtonContainer = select("#menu-button-container");
+      let offset = menuButtonContainer.offsetHeight;
 
-			window.scrollTo({
-				top: elementPos - offset,
-				behavior: "smooth",
-			});
-		};
-	});
+      let elementPos = document.querySelector("#background").offsetTop;
 
-	/* Scroll to about butoon */
-	document.querySelector(".btn-reb").addEventListener("click", (e) => {
-		e.preventDefault();
-		let menuButtonContainer = select("#menu-button-container");
-		let offset = menuButtonContainer.offsetHeight;
+      window.scrollTo({
+         top: elementPos - offset,
+         behavior: "smooth",
+      });
+   });
 
-		let elementPos = document.querySelector("#about").offsetTop;
+   select("#tooltip-hover", false).addEventListener("mouseenter", (e) => {
+      select("#tooltip-hover-content").classList.remove("d-none");
+   });
 
-		window.scrollTo({
-			top: elementPos - offset,
-			behavior: "smooth",
-		});
-	});
+   select("#tooltip-hover", false).addEventListener("mouseleave", (e) => {
+      select("#tooltip-hover-content").classList.add("d-none");
+   });
+
+   select("#tooltip-hover-2", false).addEventListener("mouseenter", (e) => {
+      select("#tooltip-hover-content-2").classList.remove("d-none");
+   });
+
+   select("#tooltip-hover-2", false).addEventListener("mouseleave", (e) => {
+      select("#tooltip-hover-content-2").classList.add("d-none");
+   });
 })();
